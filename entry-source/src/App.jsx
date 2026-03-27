03e507040bf7b1965eb0d93afbbc9bda1280243c
hzy24 <870073817@qq.com>
2026-03-27 23:54:19 +0800

    update

--- entry-source/src/App.jsx
+++ entry-source/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
 
 const App = () => {
   return (
     <div className="relative w-full overflow-x-hidden scroll-smooth">
-      <Navbar />
       <Hero />
     </div>
   );
